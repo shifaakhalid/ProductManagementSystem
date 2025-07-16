30e870c55712e295cb8296fc40f7036ef1b9d32d
shifaakhalid <154224959+shifaakhalid@users.noreply.github.com>
2025-07-16 18:21:05 +0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+          if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
     }
 }
