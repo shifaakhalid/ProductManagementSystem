8ea13b123e212e92b682cc326cf8a82587302b41
shifaakhalid <154224959+shifaakhalid@users.noreply.github.com>
2025-07-16 23:04:08 +0500

    Update and rename storeProductRequest.php to StoreProductRequest.php

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Faker\Guesser\Name;
 use Illuminate\Foundation\Http\FormRequest;
 
-class storeProductRequest extends FormRequest
+class StoreProductRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
