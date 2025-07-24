541a3621f53781ff2eac39448a7eedc5678aec3b
shifaakhalid <154224959+shifaakhalid@users.noreply.github.com>
2025-07-24 23:07:49 +0500

    Update EnsureUserIsOnboard.php

--- app/Http/Middleware/EnsureUserIsOnboard.php
+++ app/Http/Middleware/EnsureUserIsOnboard.php
@@ -8,27 +8,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 class EnsureUserIsOnboard
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-
- $user = auth()->user();
-
-    if (!$user) {
-        return redirect('/freetrial');
-       
-    }
-
-  
-      switch ($user->onboarding_step) {
-        case 'step1':
-            return redirect()->route('welcome');
-        default:
+   
             return $next($request);
-    }
-}
+    
+
 }
