ebb488845fec5e30319d1eb838a97102287b689c
shifaakhalid <154224959+shifaakhalid@users.noreply.github.com>
2025-07-24 23:09:11 +0500

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -70,8 +70,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'ensure.onboarded' => \App\Http\Middleware\EnsureUserIsOnboard::class,
-
+        
     ];
 
     
